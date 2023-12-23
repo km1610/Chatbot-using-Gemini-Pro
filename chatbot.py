d92aee8fc5a9d7692529a378a342918092e0e7a1
K. M <90254516+km1610@users.noreply.github.com>
2023-12-23 16:46:56 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -20,7 +20,7 @@ def generate():
 
 def clear():
   message_history.config(state=tk.NORMAL)
-  message_history.config(text="")
+  message_history.delete("1.0","end")
   message_history.config(state=tk.DISABLED)
 
 
@@ -33,6 +33,11 @@ message_history = tk.Text(parent, wrap=tk.WORD)
 message_history.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 message_history.config(state=tk.DISABLED)
 
+# Create a Scrollbar
+scroll = tk.Scrollbar(parent, command=message_history.yview)
+message_history.configure(yscrollcommand=scroll.set)
+scroll.grid(row=0, column=4, padx=10, pady=10)
+
 # Create an Entry widget for entering messages
 message_entry = tk.Entry(parent, width=50)
 message_entry.grid(row=1, column=0, padx=10, pady=10)
@@ -42,7 +47,7 @@ send_button = tk.Button(parent, text="Generate", command=generate, width=20, bg=
 send_button.grid(row=1, column=1, padx=10, pady=10)
 
 # Create a "Clear" button
-clear_button = tk.Button(parent, text="Clear", command=clear, width=20)
+clear_button = tk.Button(parent, text="Clear", command=clear, width=20, bg="Light Blue")
 clear_button.grid(row=1, column=2, padx=10, pady=10)
 
 # Start the Tkinter event loop
